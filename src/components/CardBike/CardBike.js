b3ea155e58027e5b50f9decf305df49c5c0faf64
Abhay singh <usernameabhay19@gmail.com>
2024-04-04 02:54:59 +0530

    api work started

--- src/components/CardBike/CardBike.js
+++ src/components/CardBike/CardBike.js
@@ -5,15 +5,16 @@ import { Link } from "react-router-dom";
 import z1000 from '../../assets/bikeImages/z1000.png'
 import NinjaLogo from '../../assets/icons/ninjaLogo.png'
 
-export default function cardBike() {
+export default function cardBike(props) {
+
     return (
         <Link to="/detailBikePage" className="CardBikeContainer h-72 w-80 m-5">
             <div className='CardBikeImage'>
                 <img src={z1000} alt="bike" />
             </div>
             <div className='CardText'>
-                <img className="w-24 h-10" src={NinjaLogo} alt="" />
-                <p>Z1000</p>
+                <img className="w-20 h-10" src={NinjaLogo} alt="" />
+                <p className="w-60 h-11 flex justify-center items-center">{props.name}</p>
             </div>
         </Link>
     )
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 import './style.css'
 import MainSlider from '../../components/MainSlider/MainSlider'
@@ -7,8 +9,31 @@ import CardBike from '../../components/CardBike/CardBike';
 import AdvertisementSlider from '../../components/AdvertisementSlider/AdvertisementSlider';
 import Footer from '../../components/Footer/Footer';
 import BikeNews from '../../components/BikeNews/BikeNews';
+import { logDOM } from '@testing-library/react';
 
 export default function Home() {
+
+    const [bikeData, setBikeData] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://api.api-ninjas.com/v1/motorcycles', {
+            params: {
+                make: 'kawasaki',
+                model: 'ninja'
+            },
+            headers: {
+                'X-Api-Key': process.env.BIKE_API_KEY || 'U2FaK0P+UIOXnRUnNs0Vkg==4FgDUMbI0PC95wS1',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                setBikeData(response.data);
+            })
+            .catch(error => {
+                console.error('Error:', error.response.data);
+            });
+    }, [])
+
     return (
         <div className='MainHomeContainer'>
             <MainNavbar />
@@ -16,14 +41,9 @@ export default function Home() {
             <div className="homepage">
                 <div className='flex justify-center'>
                     <div className='grid grid-cols-4'>
-                        <CardBike />
-                        <CardBike />
-                        <CardBike />
-                        <CardBike />
-                        <CardBike />
-                        <CardBike />
-                        <CardBike />
-                        <CardBike />
+                        {bikeData.slice(0, 8).map(e => (
+                            <CardBike name={e.model.slice(0,21)}/>
+                        ))}
                     </div>
                 </div>
                 <div className='mb-14 mt-14'>
